379aacf9894dd803a93be4ed06ddb7653bf80a59
George Nick Gorzynski <georgegorzynski@me.com>
2024-03-20 17:49:44 +0000

    Refactor routing urls

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,27 +2,35 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/:restaurantId", async (req, res) => {
-    // Get the retaurant's active orders
+router.get("/queued", async (req, res) => {
+    // Get the retaurant's queued orders (orders that have been paid but are not currently/were made)
 });
 
-router.post("/:restaurantId/receieve", async (req, res) => {
+router.get("/in-progress", async (req, res) => {
+    // Get the retaurant's in progress orders (orders that are in progress)
+});
+
+router.get("/complete", async (req, res) => {
+    // Get the retaurant's completed orders (orders that are finished)
+});
+
+router.post("/orders/receieve", async (req, res) => {
     // Receive a new order from the order microservice
 });
 
-router.get("/:restaurantId/details/:orderId", async (req, res) => {
+router.get("/details/:orderId", async (req, res) => {
     // Get the details of a specific order
 });
 
-router.put("/:restaurantId/accept/:orderId", async (req, res) => {
+router.put("/accept/:orderId", async (req, res) => {
     // Accept a new order
 });
 
-router.put("/:restaurantId/cancel/:orderId", async (req, res) => {
+router.put("/cancel/:orderId", async (req, res) => {
     // Reject a new order or cancel an accepted order
 });
 
-router.put("/:restaurantId/complete/:orderId", async (req, res) => {
+router.put("/complete/:orderId", async (req, res) => {
     // Complete an accepted order
 });
 
