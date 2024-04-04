e2c05a2866674ef9f20393bd4541ee4289e867e4
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-05 00:34:38 +0100

    Make db schema changes

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,25 +1,44 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PENDING', 'IN_PROGRESS', 'CANCELLED', 'COMPLETED', 'REJECTED');
+
 -- CreateTable
-CREATE TABLE "KitchenOrder" (
+CREATE TABLE "Order" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "restaurantId" TEXT NOT NULL,
     "orderId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
 
-    CONSTRAINT "KitchenOrder_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "KitchenOrderItem" (
+CREATE TABLE "OrderItem" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "itemName" TEXT NOT NULL,
+    "prettyName" TEXT NOT NULL,
+    "shortName" TEXT NOT NULL,
     "quantity" INTEGER NOT NULL,
-    "kitchenOrderId" TEXT,
+    "kitchenOrderId" TEXT NOT NULL,
 
-    CONSTRAINT "KitchenOrderItem_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderStatus" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "Status" NOT NULL,
+    "kitchenOrderId" TEXT NOT NULL,
+
+    CONSTRAINT "OrderStatus_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "KitchenOrderItem" ADD CONSTRAINT "KitchenOrderItem_kitchenOrderId_fkey" FOREIGN KEY ("kitchenOrderId") REFERENCES "KitchenOrder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_kitchenOrderId_fkey" FOREIGN KEY ("kitchenOrderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderStatus" ADD CONSTRAINT "OrderStatus_kitchenOrderId_fkey" FOREIGN KEY ("kitchenOrderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
