a6e930277c8cdf8e43f88defd89a27588461b2bc
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-24 19:04:39 +0100

    Modify routes and logic

--- src/routes/kitchen.routes.ts
+++ src/routes/kitchen.routes.ts
@@ -6,14 +6,18 @@ import { Order, Status } from "@prisma/client";
 
 const router = Router();
 
-router.get("/:restaurantId/orders/:status", isAuthenticated, async (req: AuthenticatedRequest, res: Response) => {
-    // Get the retaurant's queued orders (orders that have been paid but are not currently being made)
+router.get("/", async (req: AuthenticatedRequest, res: Response) => {
+    // Get the details of a specific order
     try {
-        const { restaurantId } = req.params;
+        const orders = await prisma.order.findMany({
+            include: {
+                items: true,
+                status: true,
+            },
+        });
 
-        // Filter out orders that are not pending and sort by time
         const status = () => {
-            switch (req.params.status) {
+            switch (req.query.status) {
                 case "in-progress":
                     return "IN_PROGRESS";
                 case "completed":
@@ -24,56 +28,26 @@ router.get("/:restaurantId/orders/:status", isAuthenticated, async (req: Authent
                     return null;
             }
         };
-        if (!status) {
-            res.status(400).json({
-                error: {
-                    message: "Invalid status"
-                }
-            });
-            return;
+
+        if (status) {
+            orders.filter((order: any) => order.status.status === status);
         }
-
-        // Fetch all orders, including the status
-        let orders = await prisma.order.findMany({
-            where: {
-                restaurantId,
-            },
-            include: {
-                status: true,
-            },
-        });
-
-        if (!orders) {
-            res.status(404).json({
-                error: {
-                    message: "No orders found"
-                }
-            });
-            return;
-        }
-
-        if (!status) {
-            // Filter orders by desired status
-            orders = orders.filter((order: any) => order.status.status === status);
-        }
-
-        // Sort orders by recency
-        orders = orders.sort((a: Order, b: Order) => parseInt((a.createdAt.getTime() / 1000).toFixed(0)) - parseInt((b.createdAt.getTime() / 1000).toFixed(0)));
 
         res.status(200).json({
             data: orders
-        })
+        });
     } catch (error) {
         res.status(500).json({
             error: {
-                message: "Failed to get pending orders",
+                message: "Failed to get order details",
                 details: error
             }
         });
+    
     }
 });
 
-router.get("/:orderId/details", orderExists, async (req: AuthenticatedRequest, res: Response) => {
+router.get("/:orderId", orderExists, async (req: AuthenticatedRequest, res: Response) => {
     // Get the details of a specific order
     try {
         const { orderId } = req.params;
@@ -111,28 +85,25 @@ router.get("/:orderId/details", orderExists, async (req: AuthenticatedRequest, r
     }
 });
 
-router.put("/:orderId/status/:newStatus", isAuthenticated, hasRole("restaurant"), orderExists, async (req: AuthenticatedRequest, res: Response) => {
+router.put("/:orderId/:newStatus", isAuthenticated, hasRole("restaurant"), orderExists, async (req: AuthenticatedRequest, res: Response) => {
     try {
         const { orderId } = req.params;
 
         // Determine the new status
         let newStatus: Status = null;
         switch (req.params.newStatus) {
-            case "accept":
-                newStatus = Status.IN_PROGRESS; // Order is being made
-                break;
             case "complete":
                 newStatus = Status.COMPLETED; // Order is ready to be taken to customer/fulfilled
                 break;
             case "cancel":
-                newStatus = Status.CANCELLED; // Order is cancelled after being accepted
+                newStatus = Status.CANCELLED; // Order is to be cancelled
                 break;
         };
         if (!newStatus) {
             res.status(400).json({
                 error: {
                     message: "Invalid status",
-                    details: "Valid statuses are 'accept', 'complete', 'cancel', 'reject'."
+                    details: "Valid statuses are 'complete' or 'cancel'."
                 }
             });
             return;
