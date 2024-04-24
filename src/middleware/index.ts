f2bee1ae25bc67a807291a086691ca40579db2ab
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-25 00:23:32 +0100

    Correctly export middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,3 +2,4 @@ export { default as decodeJWT } from "./decode_jwt";
 export { default as isAuthenticated } from "./authenticated";
 export { default as hasRole } from "./has_role";
 export { default as orderExists } from "./order_exists";
+export { ownsRestaurant } from "./is_restaurant_owner";
